58fd39709ae28c02e497b991fcd26b8b523e0bca
wesleyafonsogomes <wesleyafonsogomes@yahoo.com.br>
2025-12-16 20:23:34 -0300

    att

--- 7_TEMPLATE_ENGINE/index.js
+++ 7_TEMPLATE_ENGINE/index.js
@@ -6,10 +6,48 @@ const app = express()
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
 
+
+// css
+
+app.use(express.static("public"))
+
 // rota inicial 
 
+app.get("/dashboard", (req,res) => {
+
+    const itens = ["Item A", "Item B", "Item C"]
+
+    res.render("dashboard", {itens})
+})
+
+// post 
+
+app.get("/post", (req,res) => {
+
+    const post = {
+        title: "Aprender Node.js",
+        category: "JavaScript",
+        body: "Este artigo vai te ajudar a aprender Node.js...",
+        comments: 4,
+    }
+
+    res.render("blogpost", {post})
+
+}) 
+
 app.get("/", (req,res) => {
-    res.render("home", { layout: false })
+
+    const user = {
+        name: "Wesley",
+        surname: "Afonso",
+        age: 30
+    }
+
+    const auth = true
+
+    const approved = true
+
+    res.render("home", {user, auth, approved})
 })
 
 app.listen(3000, () => {
